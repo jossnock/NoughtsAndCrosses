90ce35d929af8de6f2f01d78bc220fc7ac3c0082
JossNockolds <19121867@hlnsc.ac.uk>
2024-06-18 11:50:17 +0100

    works and almost works for dynamic-sized board

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,82 +1,136 @@
 ﻿using System.Diagnostics;
 /*
 To-do:
- - Generalise the functions to work with n*m size board
- - 
+ - Generalise the functions to work with n-by-n size board
+     - Select board size
+ - Improve checking system:
+     - Check top left corner for matches in adjacent cells (proceed if match, else break)
+     - Check opposite corner for matches in adjacent cells (proceed if match, else break)
+     - Check top right corner for anti-diagonal matches
+     - Check centre for row/column matches
 */
 
 // defining methods:
 
-static void NCPrint(char[,] inputBoard, int BoardHeight, int BoardWidth)
+static void NCPrint(char[,] inputBoard)
+/* 
+Prints the current state of the board.
+*/
+{
+    int BoardLength = inputBoard.GetLength(0); // BoardWidth = BoardHeight b/c board will always be a square
+
+    // Dynamically constructing rowSeparators:
+
+    string rowTop = "┌─";
+    for (int i = 0; i < BoardLength - 1; i++) rowTop += "──┬─";
+    rowTop += "──┐";
+    // So for BoardLength = 2, rowTop = "┌───┬───┐" (allowing 2 spots for sigils per row)
+
+    string rowMiddle = "├─";
+    for (int i = 0; i < BoardLength - 1; i++) rowMiddle += "──┼─";
+    rowMiddle += "──┤";
+    // So for BoardLength = 5, rowMiddle = "├───┼───┼───┼───┼───┤"
+
+    string rowBottom = "└─";
+    for (int i = 0; i < BoardLength - 1; i++) rowBottom += "──┴─";
+    rowBottom += "──┘";
+    // So for BoardLength = 3, rowBottom = "└───┴───┴───┘"
+
+    // Creating the rowSeparators Array:
+    string[] rowSeparators = new string[BoardLength + 1]; // + 1 to account for rowBottom
+    rowSeparators[0] = rowTop;
+    for (int i = 1; i < BoardLength; i++) rowSeparators[i] = rowMiddle; // Populates array with (BoardLength - 1) 'rowMiddle's
+    rowSeparators[BoardLength] = rowBottom;
+    // So for BoardLength = 4, rowSeparators = { "┌───┬───┬───┬───┐",
+    //                                           "├───┼───┼───┼───┤",
+    //                                           "├───┼───┼───┼───┤",
+    //                                           "├───┼───┼───┼───┤",
+    //                                           "└───┴───┴───┴───┘" } 
+
+
+    for (int row = 0; row < BoardLength; row++)
     {
-        // Prints the current state of the board:
-
-        string[] RowSeparators = { "┌───┬───┬───┐", "├───┼───┼───┤", "├───┼───┼───┤", "└───┴───┴───┘" };
-
-        for (int row = 0; row < BoardHeight; row++)
+        Console.WriteLine(rowSeparators[row]);
+        for (int column = 0; column < BoardLength; column++)
         {
-            Console.WriteLine(RowSeparators[row]);
-            for (int column = 0; column < BoardWidth; column++)
-            {
-                Console.Write("| "); // separator between each char
-                Console.Write(inputBoard[row, column]);
-                Console.Write(' '); // spacing after each char
-            }
-            Console.WriteLine('|'); // separator between eand char + new line after each row
+            Console.Write("| "); // separator between each char
+            Console.Write(inputBoard[row, column]);
+            Console.Write(' '); // spacing after each char
         }
-        Console.WriteLine(RowSeparators[BoardWidth]);
+        Console.WriteLine('|'); // separator between end char and makes a new line after each row
+    }
+    Console.WriteLine(rowSeparators[BoardLength]);
+}
+
+
+static char CheckForWin(char[,] inputBoard)
+/*
+Checks if, in the given board, a player has won.
+Returns the winning player's sigil (e.g. 'X').
+*/
+{
+    int BoardLength = inputBoard.GetLength(0); // BoardWidth = BoardHeight b/c board will always be a square
+
+    // Horizontal:
+    for (int row = 0; row < BoardLength; row++)
+    {
+        for (int column = 1; column < BoardLength; column++) // starts on 1 to not check if inputBoard[row, 0] != itself
+        {
+            if (inputBoard[row, 0] != inputBoard[row, column]) break; // stop checking row if not matching
+            if (column == BoardLength - 1) return inputBoard[row, 0]; // if after all the checks the statement hasn't been broken out of, they must all match
+        }        
     }
 
-    static char CheckForWin(char[,] inputBoard, int BoardHeight, int BoardWidth)
+    // Vertical:
+    for (int column = 0; column < BoardLength; column++)
     {
-        // Horizontal:
-        for (int row = 0; row < BoardHeight; row++)
+        for (int row = 1; row < BoardLength; row++) // starts on 1 to not check if inputBoard[0, column] != itself
         {
-            if ((inputBoard[row, 0] == inputBoard[row, 1]) & (inputBoard[row, 1] == inputBoard[row, 2]) & (inputBoard[row, 0] != ' ')) // if all chars in row are the same and row is not empty
-            {
-                return inputBoard[row, 0];
-            }
-        }
-
-        // Vertical:
-        for (int column = 0; column < BoardWidth; column++)
-        {
-            if ((inputBoard[0, column] == inputBoard[1, column]) & (inputBoard[1, column] == inputBoard[2, column]) & (inputBoard[0, column] != ' ')) // if all chars in column are the same and column is not empty
-            {
-                return inputBoard[0, column];
-            }
-        }
-
-        // Main diagonal:
-        if ((inputBoard[0, 0] == inputBoard[1, 1]) & (inputBoard[1, 1] == inputBoard[2, 2]) & (inputBoard[0, 0] != ' ')) // if all chars in diagonal are the same and diagonal is not empty
-        {
-            return inputBoard[0, 0];
-        }
-
-        // Anti-diagonal:
-        if ((inputBoard[0, 2] == inputBoard[1, 1]) & (inputBoard[1, 1] == inputBoard[2, 0]) & (inputBoard[0, 2] != ' ')) // if all chars in diagonal are the same and diagonal is not empty
-        {
-            return inputBoard[0, 2];
-        }
-        return ' '; // neither player has won yet
+            if (inputBoard[0, column] != inputBoard[row, column]) break; // stop checking column if not matching
+            if (row == BoardLength - 1) return inputBoard[0, column]; // if after all the checks the statement hasn't been broken out of, they must all match
+        }        
     }
+
+    // Main diagonal:
+    for (int coords = 1; coords < BoardLength; coords++) // starts on 1 to not check if inputBoard[0, 0] != itself
+    {
+        if (inputBoard[0, 0] != inputBoard[coords, coords]) break; // stop checking diagonal if not matching
+        if (coords == BoardLength - 1) return inputBoard[0, 0]; // if after all the checks the statement hasn't been broken out of, they must all match
+    }
+
+    // Anti-diagonal:
+    for (int row = 1, column = BoardLength - 2; (row < BoardLength) & (column >= 0); row++, column--) // starts on 1 and BoardLength - 2 to not check if inputBoard[0, BoardLength - 1] != itself
+    {
+        if (inputBoard[0, BoardLength - 1] != inputBoard[row, column]) break; // stop checking diagonal if not matching
+        if (row == BoardLength - 1) return inputBoard[0, BoardLength - 1]; // if after all the checks the statement hasn't been broken out of, they must all match
+    }
+
+    // default case:
+    return ' '; // neither player has won yet
+}
+
 
 // Playing the game:
+
+// Dynamically setting boardSize and defining board:
 
 char[,] board = { { '1', '2', '3' }, 
                   { '4', '5', '6' }, 
                   { '7', '8', '9' } };
-int BoardHeight = 3, BoardWidth = 3;
+
+
+
+
 int currentPlayer = 0; // 0 = player 1 because it makes converting from player number to X or O and switching between p1 and p2 easier
 char[] Players = { 'X', 'O' };
 int turnNum = 1;
 
-while (CheckForWin(board, BoardHeight, BoardWidth) == ' ')
+while (CheckForWin(board) == ' ')
 {
-    NCPrint(board, BoardHeight, BoardWidth);
+    NCPrint(board);
     Console.WriteLine($"- - - Player {currentPlayer + 1}'s turn - - -");
 
-    Console.WriteLine($"Player {currentPlayer + 1}, choose a space (1-9): "); // since currentPlayer is either 0 or 1, 1 must be added
+    Console.WriteLine($"Player {currentPlayer + 1}, choose a space (1-9): "); // since currentPlayer starts on 0, 1 must be added
     char space = Console.ReadLine()[0]; // convert input string of 1 letter to char
 
     Console.WriteLine("- - - - - - - - - - - - - -"); // buffer between rounds
@@ -87,6 +141,7 @@ while (CheckForWin(board, BoardHeight, BoardWidth) == ' ')
         continue; // skips the switch between players and the board update
     }
 
+    // converting space name to corresponding row and column:
     int row = 0, column = 0; // defining vars in current scope to change them in switch statement
     switch (space)
     {
@@ -118,7 +173,7 @@ while (CheckForWin(board, BoardHeight, BoardWidth) == ' ')
             break;
     }
 
-    if (!Char.IsDigit(board[row, column])) // if the space is already filled
+    if (!Char.IsDigit(board[row, column])) // if the space is already filled with a sigil
     {
         Console.WriteLine("That space is already filled, choose an empty space");
         continue; // skips the switch between players and the board update
@@ -137,33 +192,10 @@ while (CheckForWin(board, BoardHeight, BoardWidth) == ' ')
     }
 }
 
-NCPrint(board, BoardHeight, BoardWidth);
+NCPrint(board);
 
-char winner = CheckForWin(board, BoardHeight, BoardWidth);
+char winner = CheckForWin(board);
 
 if (winner == 'X') Console.WriteLine("Player 1 wins!");
 else if (winner == 'O') Console.WriteLine("Player 2 wins!");
 else Console.WriteLine("Draw!");
-
-
-// Asserts:
-
-char[,] tempBoard1 = { { 'O', 'O', 'O' }, 
-                       { ' ', 'X', ' ' }, 
-                       { 'X', ' ', 'X' } };
-Debug.Assert(CheckForWin(tempBoard1, BoardHeight, BoardWidth) == 'O'); // Horizontal
-
-char[,] tempBoard2 = { { 'O', 'X', 'O' }, 
-                       { ' ', 'X', ' ' }, 
-                       { ' ', 'X', ' ' } };
-Debug.Assert(CheckForWin(tempBoard2, BoardHeight, BoardWidth) == 'X'); // Vertical
-
-char[,] tempBoard3 = { { 'X', 'O', ' ' }, 
-                       { ' ', 'X', ' ' }, 
-                       { 'O', ' ', 'X' } };
-Debug.Assert(CheckForWin(tempBoard3, BoardHeight, BoardWidth) == 'X'); // Main diagonal
-
-char[,] tempBoard4 = { { 'X', 'X', 'O' }, 
-                       { 'X', 'O', ' ' }, 
-                       { 'O', ' ', ' ' } };
-Debug.Assert(CheckForWin(tempBoard3, BoardHeight, BoardWidth) == 'O'); // Anti-diagonal
